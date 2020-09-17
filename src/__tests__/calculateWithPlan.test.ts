2d68649c603cf03137cfabebfb2dc10b516fda24
GabrielModog <gabrielmodog@gmail.com>
2020-09-17 14:45:02 -0300

    add more calculate with plan tests

--- src/__tests__/calculateWithPlan.test.ts
+++ src/__tests__/calculateWithPlan.test.ts
@@ -3,27 +3,39 @@ import calls from '../data/calls.json';
 import planCalls from '../data/plans.json';
 
 describe('Calls with FaleMais plans:', () => {
-  test('Should return 0 if FaleMais30 plan call is from 011 - 016', () => {
+  test('Should return 0 if FaleMais30 plan call is from 011 - 016 past 20 minutes', () => {
     expect(calculateWithPlan(calls['011']['016'], 20, planCalls[0].value)).toBe(
       0
     );
   });
 
-  test('Should return 37.4 if FaleMais60 plan call is from 011 - 017', () => {
+  test('Should return 37.4 if FaleMais60 plan call is from 011 - 017 past 80 minutes', () => {
     expect(calculateWithPlan(calls['011']['017'], 80, planCalls[1].value)).toBe(
       37.4
     );
   });
 
-  test('Should return 167.2 if FaleMais120 plan call is from 018 - 011', () => {
+  test('Should return 167.2 if FaleMais120 plan call is from 018 - 011 past 200 minutes', () => {
     expect(
       calculateWithPlan(calls['018']['011'], 200, planCalls[2].value)
     ).toBe(167.2);
   });
 
-  test('Should return 9.0 if FaleMais60 plan call is from 011 - 018', () => {
+  test('Should return 9.9 if FaleMais60 plan call is from 011 - 018 past 70 minutes', () => {
     expect(calculateWithPlan(calls['011']['018'], 70, planCalls[1].value)).toBe(
-      9.0
+      9.9
     );
+  });
+
+  test('Should return 39.6 if FaleMais120 plan call is from 011 - 018 past 160 minutes', () => {
+    expect(
+      calculateWithPlan(calls['011']['018'], 160, planCalls[2].value)
+    ).toBe(39.6);
+  });
+
+  test('Should return 29.7 if FaleMais120 plan call is from 011 - 018 past 130 minutes', () => {
+    expect(
+      calculateWithPlan(calls['011']['018'], 130, planCalls[2].value)
+    ).toBe(29.7);
   });
 });
