b40d1ba5f7a41fc11dad6832e1320a2c46fba9a9
GabrielModog <gabrielmodog@gmail.com>
2020-09-20 11:21:43 -0300

    valids and add feature to calls component

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,38 @@
-import React from 'react';
-import { Col, Row, Container, Form } from 'react-bootstrap';
-import Prices from './components/Prices';
+import React, { useState } from 'react';
+import { Container } from 'react-bootstrap';
 
-const App: React.FC = () => (
-  <Container className="w-50" fluid="sm">
-    <h1 className="text-center py-4">FaleMais</h1>
-    <Row>
-      <Form className="w-100">
-        <Row className="justify-content-md-center">
-          <Col md>
-            <Form.Group>
-              <Form.Label>Origem</Form.Label>
-              <Form.Control as="select" size="lg" custom>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </Form.Control>
-            </Form.Group>
-          </Col>
-          <Col md>
-            <Form.Group>
-              <Form.Label>Destino</Form.Label>
-              <Form.Control as="select" size="lg" custom>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </Form.Control>
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row className="justify-content-md-center">
-          <Col md>
-            <Form.Group>
-              <Form.Label>Minutos</Form.Label>
-              <Form.Control type="number" />
-            </Form.Group>
-          </Col>
-          <Col md>
-            <Row className="align-items-center justify-content-around h-100">
-              <Form.Check inline label="FaleMais30" type="radio" />
-              <Form.Check inline label="FaleMais60" type="radio" />
-              <Form.Check inline label="FaleMais120" type="radio" />
-            </Row>
-          </Col>
-        </Row>
-      </Form>
-    </Row>
-    <Prices />
-    <h6 className="text-center font-weight-light pt-4">Telzir</h6>
-  </Container>
-);
+import calls from './data/calls.json';
+import plans from './data/plans.json';
+
+import Prices from './components/Prices';
+import CallsRecord from './components/CallsRecord';
+
+const App: React.FC = () => {
+  const [callInputs, setCallInputs] = useState<any>({
+    minutes: 1,
+  });
+
+  const onChangeForms = (event: any): void => {
+    const { value, name } = event.target;
+    setCallInputs({ ...callInputs, [name]: value });
+  };
+
+  return (
+    <Container className="w-50" fluid="sm">
+      <h1 className="text-center py-4">FaleMais</h1>
+      <CallsRecord
+        calls={calls}
+        plans={plans}
+        origin={callInputs?.origin}
+        destination={callInputs?.destination}
+        minutes={callInputs?.minutes}
+        planCheck={callInputs?.planCheck}
+        onChangeForms={onChangeForms}
+      />
+      <Prices />
+      <h6 className="text-center font-weight-light pt-4">Telzir</h6>
+    </Container>
+  );
+};
 
 export default App;
---
+++ src/components/CallsRecord.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import { Row, Form, Col } from 'react-bootstrap';
+
+interface ICallsRecord {
+  calls: any;
+  plans: any;
+  origin: number;
+  destination: number;
+  minutes: number;
+  planCheck: string;
+  onChangeForms: (event: any) => void;
+}
+
+const CallsRecord = ({
+  calls,
+  plans,
+  origin,
+  destination,
+  minutes,
+  planCheck,
+  onChangeForms,
+}: ICallsRecord) => {
+  return (
+    <Row>
+      <Form className="w-100">
+        <Row className="justify-content-md-center">
+          <Col md>
+            <Form.Group>
+              <Form.Label>Origem</Form.Label>
+              <Form.Control
+                as="select"
+                name="origin"
+                size="lg"
+                onChange={(evt: any) => onChangeForms(evt)}
+                custom
+              >
+                <option value="">Selecione o DDD</option>
+                {Object.keys(calls).map(originCall => (
+                  <option value={originCall} key={originCall}>
+                    {originCall}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+          </Col>
+          <Col md>
+            <Form.Group>
+              <Form.Label>Destino</Form.Label>
+              <Form.Control
+                as="select"
+                name="destination"
+                size="lg"
+                disabled={Boolean(!origin)}
+                onChange={(evt: any) => onChangeForms(evt)}
+                custom
+              >
+                <option value="">
+                  {origin
+                    ? 'Selecione o DDD do destinado'
+                    : 'Aguardando DDD de origem'}
+                </option>
+                {origin &&
+                  Object.keys(calls[origin]).map(item => (
+                    <option value={item} key={item}>
+                      {item}
+                    </option>
+                  ))}
+              </Form.Control>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col md>
+            <Form.Group>
+              <Form.Label>Minutos</Form.Label>
+              <Form.Control
+                type="number"
+                name="minutes"
+                defaultValue={minutes}
+                disabled={Boolean(!destination)}
+                onChange={(evt: any) => onChangeForms(evt)}
+                min={1}
+              />
+            </Form.Group>
+          </Col>
+          <Col md>
+            <Row className="align-items-center justify-content-around h-100">
+              {plans.map((plan: any) => (
+                <div key={plan.name}>
+                  <Form.Check
+                    inline
+                    label={plan.name}
+                    type="radio"
+                    name="planCheck"
+                    value={plan.value}
+                    onChange={(evt: any) => onChangeForms(evt)}
+                    disabled={Boolean(!destination)}
+                  />
+                </div>
+              ))}
+            </Row>
+          </Col>
+        </Row>
+      </Form>
+    </Row>
+  );
+};
+
+export default CallsRecord;
